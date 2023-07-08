5c590709507ba0f7aad5028cc8e2b2bc47e62d07
Sergei Astapkovich <astapkovich.95@mail.ru>
2023-07-08 11:24:16 +0500

    task 1-2

---
+++ Task1_2.java
@@ -0,0 +1,9 @@
+package Dz;
+
+public class Task1_2 {
+    public static void main(String[] agrs) {
+        for (int i = 1; i <=  1000; i++) {
+            System.out.println(i);
+        }
+    }
+}
